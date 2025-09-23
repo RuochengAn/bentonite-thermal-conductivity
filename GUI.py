138410eec08c126298e4b935c132ee3b9c4a1a3b
Ruocheng An <101235394+RRRRRAVENNNNN@users.noreply.github.com>
2025-09-23 11:15:21 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -16,16 +16,16 @@ ss = joblib.load('StandardScaler.joblib')
 col1, col2 = st.columns(2)
 
 with col1:
-    feature1 = st.number_input(u'$\mathrm{Temperature}$', step=0.01, format='%.2f')
-    feature2 = st.number_input(u'$\mathrm{Water\;content}$', step=0.01, format='%.2f')
-    feature3 = st.number_input(u'$\mathrm{Dry\;density}$', step=0.01, format='%.2f')
-    feature4 = st.number_input(u'$\mathrm{Montmorillonite\;content}$', step=0.01, format='%.2f')
+    feature1 = st.number_input(u'$\mathrm{Temperature\;(℃)}$', step=0.01, format='%.2f')
+    feature2 = st.number_input(u'$\mathrm{Water\;content\;(\%)}$', step=0.01, format='%.2f')
+    feature3 = st.number_input(u'$\mathrm{Dry\;density\;(g/cm^3)}$', step=0.01, format='%.2f')
+    feature4 = st.number_input(u'$\mathrm{Montmorillonite\;content\;(\%)}$', step=0.01, format='%.2f')
 
 with col2:
-    feature5 = st.number_input(u'$\mathrm{Liquid\;limit}$', step=0.01, format='%.2f')
-    feature6 = st.number_input(u'$\mathrm{Plastic\;limit}$', step=0.01, format='%.2f')
-    feature7 = st.number_input(u'$\mathrm{Graphite\;content}$', step=0.01, format='%.2f')
-    feature8 = st.number_input(u'$\mathrm{Sand\;content}$', step=0.01, format='%.2f')
+    feature5 = st.number_input(u'$\mathrm{Liquid\;limit\;(\%)}$', step=0.01, format='%.2f')
+    feature6 = st.number_input(u'$\mathrm{Plastic\;limit\;(\%)}$', step=0.01, format='%.2f')
+    feature7 = st.number_input(u'$\mathrm{Graphite\;content\;(\%)}$', step=0.01, format='%.2f')
+    feature8 = st.number_input(u'$\mathrm{Sand\;content\;(\%)}$', step=0.01, format='%.2f')
     
 feature_values = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8]
 
